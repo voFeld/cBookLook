7218c94e1ac28211372d8ca1fb5c39150084ded6
voFeld <vofeld@gmail.com>
2016-08-03 16:31:09 +0300

    fineshed project

--- script.js
+++ script.js
@@ -1,141 +1,37 @@
-var formIsValid = false;
-//From HTML
-var bookTitlePlaceholder = "Book Title";
-var bookAuthorPlaceholder = "Book Author";
-var bookDescrPlaceholder = "Book Description";
-var imgURLPlaceholder = "Image URL";
-var numPagesPlaceholder = "Number of Pages";
-var mpdPlaceholder = "Minutes You Read per Day";
+var source = $('#book-template').html();
+var template = Handlebars.compile(source);
 
-//Error messages
-var missingTextMsg = "This field must be filled out";
-var notNumericMsg = "Must be a whole number";
-var badImageTypeMsg = "This is not a valid image URL, please choose a proper link";
+$('button').on('click', function(e){
+    e.preventDefault();
+    $('.present-book').empty();
 
-//To track the x's and v's in the validation
-var validatedInputs = [];
-var unValidatedInputs = [];
-var validImageExtensions = ["png","tiff","jpg","jpeg","bmp","svg"];
-
-$('#formSubmitBtn').click(function() {
-
-    var bookTitle = $("#formBookTitleID").val();
-    var bookAuthor = $("#formBookAuthorID").val();
-    var bookDescr = $("#formBookDescriptionID").val();
-    var bookImgURL = $("#formImageURLID").val();
-    var bookNumPages = $("#formNumPagesID").val();
-    var bookMPD = $("#formMPDID").val();
-
-    var daysToRead = getDaysToRead(bookNumPages, bookMPD);
-
-    if(formIsValid){
-        displayOnHTML(daysToRead, bookTitle, bookDescr, bookAuthor, bookImgURL);
-        clearForm();
-        formIsValid = false;
-    }
-    else{
-        alert("Please correct the errors");
-    }
+    fetch();
 });
 
-function clearForm(){
-    $('form').find("input[type=text], textarea").val("");
-    $('form').find(".fa-check").removeClass("fa-check-show");
-    validatedInputs = [];//reset so that checkmarks can come back up
-}
+//Fetch the JSON
+var fetch = function () {
+    $.ajax({
+        method: "GET",
+        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + $('#title-key').val(),
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
 
-//Add/remove x's and v's for validation
-$("input").on("keyup",  function(){
+            var newHTML = template({
+                title: data.items[0].volumeInfo.title,
+                author: data.items[0].volumeInfo.authors[0],
+                description: data.items[0].volumeInfo.description,
+                imgurl: data.items[0].volumeInfo.imageLinks.thumbnail,
+                numpages: data.items[0].volumeInfo.pageCount,
+                minday: $('#min-day').val()
+            });
 
-    var placeholder = this.placeholder;	//the placeholder text used as an identifier
-    var input = $(this).val();
 
-    if(placeholder == bookTitlePlaceholder || placeholder == bookDescrPlaceholder || placeholder == bookAuthorPlaceholder){
 
-        if(input.length < 1){
-            showError(this, missingTextMsg);
+            $('.present-book').append(newHTML);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
         }
-        else{
-            showCheck(this);
-        }
-    }
-
-    if(placeholder == mpdPlaceholder || placeholder == numPagesPlaceholder){
-        if(Math.floor(input) == input && $.isNumeric(input)){
-            showCheck(this);
-        }
-        else{
-            showError(this, notNumericMsg);
-        }
-    }
-
-    if(placeholder == imgURLPlaceholder){
-        var afterPeriods = input.split(".");
-        var extension = afterPeriods.pop();
-
-        if(afterPeriods.length > 0 && $.inArray(extension, validImageExtensions) > -1){
-            showCheck(this);
-        }
-        else{
-            showError(this,badImageTypeMsg);
-        }
-    }
-
-    var numErrors = $('.fa-times-show').length;
-    if(numErrors == 0){
-        formIsValid = true;
-    }
-
-});
-
-function showCheck(input){
-    //If the input has not been validated yet, toggle the class to show the green tick
-    if($.inArray(input.id, validatedInputs) == -1){
-        $(input).next(".fa-check").toggleClass("fa-check-show");
-        validatedInputs.push(input.id);
-    }
-    //remove x if it exists
-    var errorIndex = $.inArray(input.id, unValidatedInputs);
-    if(errorIndex > -1){
-        $(input).next().next(".fa-times").toggleClass("fa-times-show");
-        unValidatedInputs.splice(errorIndex,1);
-    }
-}
-
-function showError(input, errorMessage){
-    //If x hasn't been displayed yet, toggle the class to show the green tick
-    if($.inArray(input.id, unValidatedInputs) == -1){
-        $(input).next().next(".fa-times").toggleClass("fa-times-show");
-        unValidatedInputs.push(input.id);
-
-        //Clear, then add the error message
-        $(input).next().next("i").find('span').remove();
-        $(input).next().next("i").append("<span> "+errorMessage+"</span>");
-    }
-    //remove check if it exists
-    var checkIndex = $.inArray(input.id, validatedInputs);
-    if(checkIndex > -1){
-        $(input).next(".fa-check").toggleClass("fa-check-show");
-        validatedInputs.splice(checkIndex,1);
-    }
-}
-
-function displayOnHTML(daysToRead, bookTitle, bookDescr, bookAuthor, bookImgURL){
-    $('#result').empty();
-    // turn our "template" into html
-    var source = $('#bookDisplay-template').html();
-
-    // compile our template html using handlebars
-    var template = Handlebars.compile(source);
-
-    // fill our template with information
-    var newHTML = template({daysToRead: daysToRead, bookTitle: bookTitle, bookDescr:bookDescr, bookAuthor: bookAuthor, bookImgURL: bookImgURL});
-
-    // append our new html to the page
-    $('#result').html(newHTML);
-}
-
-function getDaysToRead(numPages, mpd){
-    //Assume 1 minute = 2 page
-    return daysToRead = (numPages / (mpd*2))
-}
+    });
+};
